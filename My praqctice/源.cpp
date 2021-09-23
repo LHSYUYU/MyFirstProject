69e534f4b675dd8f80c326132da20dc71a01f3ba
LHSYUYU <90950318+LHSYUYU@users.noreply.github.com>
2021-09-23 23:43:03 +0800

    Add files via upload

---
+++ My praqctice/源.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << " My name is LHS amd Iam from Maoming";
+	cout << endl;
+	cout << "First,I study C++" << endl;
+	return 0;
+
+
+}
